1329999080765378473aa9c1a27fe2c3fba9f9ff
adelnizamutdinov <stiggpwnz@gmail.com>
2022-04-13 22:51:36 +0400

    amen

--- env.test.ts
+++ env.test.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "vitest";
 
+test.concurrent("NODE_ENV", () => {
+  expect(process.env.NODE_ENV).toEqual("test");
+});
+
 test.concurrent(".env.test", () => {
   expect(process.env.FOO).toEqual("BAR");
 });
